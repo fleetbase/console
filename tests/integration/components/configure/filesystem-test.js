3985be51722627f7da737ddb8f2592665a13b6b1
Ronald A. Richardson <me@ron.dev>
2023-06-08 18:41:47 +0800

    completed admin system configuration

--- tests/integration/components/configure/filesystem-test.js
+++ tests/integration/components/configure/filesystem-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from '@fleetbase/console/tests/helpers';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | configure/database', function (hooks) {
+module('Integration | Component | configure/filesystem', function (hooks) {
     setupRenderingTest(hooks);
 
     test('it renders', async function (assert) {
         // Set any properties with this.set('myProperty', 'value');
         // Handle any actions with this.set('myAction', function(val) { ... });
 
-        await render(hbs`<Configure::Database />`);
+        await render(hbs`<Configure::Filesystem />`);
 
         assert.dom(this.element).hasText('');
 
         // Template block usage:
         await render(hbs`
-      <Configure::Database>
+      <Configure::Filesystem>
         template block text
-      </Configure::Database>
+      </Configure::Filesystem>
     `);
 
         assert.dom(this.element).hasText('template block text');
